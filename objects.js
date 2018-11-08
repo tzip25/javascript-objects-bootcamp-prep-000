0d6467d8d784f213714a814e5a2f91e388bc957c
Tali Scheer <tali.scheer@gmail.com>
2018-11-08 13:33:19 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  Greenday: "American Idiot",
+  Sublime: "What I Got",
+};
+
+function updatePlaylist(obj, key, value) {
+obj[key] = value;
+return obj;
+}
+
+function removeFromPlaylist(obj, key) {
+  delete obj.key;
+  return obj;
+}
